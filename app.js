0f9e9f5768581dc7613b4d22c0b51b278e7084a5
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-06-27 10:25:30 +0500

    i added edit and file upload feature

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ let postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 let jwt = require("jsonwebtoken");
+const multer = require("multer");
+let crypto = require("crypto");
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -23,6 +40,39 @@ app.get("/profile", isLoggedIn, async (req, res) => {
     .findOne({ email: req.user.email })
     .populate("posts");
   res.render("profile", { user });
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  if (post.likes.indexOf(req.user.userId) === -1) {
+    post.likes.push(req.user.userId);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userId), 1);
+  }
+  await post.save();
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id });
+  res.render("edit", { post });
+});
+
+app.get("/test", isLoggedIn, (req, res) => {
+  res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+
+  res.redirect("/profile");
 });
 
 app.post("/post", isLoggedIn, async (req, res) => {
@@ -40,6 +90,7 @@ app.get("/logout", (req, res) => {
   res.cookie("token", "");
   res.redirect("login");
 });
+
 app.post("/register", async (req, res) => {
   let { email, password, username, name, age } = req.body;
   let user = await userModel.findOne({ email });
